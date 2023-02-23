808ad596844512b3830e3ce51629c3bdfe48657b
ucbooks.education <krishnas55@live.com>
2023-02-23 17:46:13 +0100

    initial commit

---
+++ reasoner.py
@@ -0,0 +1,10 @@
+from ast import arg
+from rdflib import Graph, RDFS, RDF, URIRef, Namespace, Literal, XSD
+from owlrl import DeductiveClosure, RDFS_Semantics
+import random, sys
+import sys
+import os
+import pandas as pd
+from string import Template
+import re
+import json
