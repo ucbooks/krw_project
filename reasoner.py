0737b53810b03044192e8e52ac563b7f5d7642a9
ucbooks.education <krishnas55@live.com>
2023-02-23 18:00:50 +0100

    initia commit

--- reasoner.py
+++ reasoner.py
@@ -8,3 +8,29 @@ import pandas as pd
 from string import Template
 import re
 import json
+
+
+def reasoner(turtle_file):
+
+    """
+        Takes in a graph and performs a search on dbpedia for the unknown classes. 
+    """
+
+    with open(turtle_file, "r") as hander:
+        turtle_data = handler.read()
+
+    # Split the statements by fullstop.
+    statements = turtle_data.split(".")
+
+    # Strip statements
+    stripped_statements = []
+    for statement in statements:
+        stripped_statements.append(
+            statement.strip()
+        )
+
+    # Turn statements into (subject, predicate, object) tuples
+    spo_tuples = []
+
+    
+    
