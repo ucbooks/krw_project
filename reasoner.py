9acda98066b6d2f352cba744036d4fa6cbd059ae
ucbooks.education <krishnas55@live.com>
2023-03-02 15:03:00 +0100

    wikidata search isn't working fully

--- reasoner.py
+++ reasoner.py
@@ -1,5 +1,5 @@
 from ast import arg
-from rdflib import Graph, RDFS, RDF, URIRef, Namespace, Literal, XSD, FOAF
+from rdflib import Graph, RDFS, RDF, URIRef, Namespace, Literal, XSD, FOAF, OWL
 from owlrl import DeductiveClosure, RDFS_Semantics
 import random, sys
 import sys
@@ -23,50 +23,191 @@ truth assesment percentages or over subgraphs
 def reasoner(turtle_file):
 
     """
-        Check the validity of the turtle triples
-           - input: Turtle file
-           - output: Consistency booleans for each triple
+        Check the validity of the turtle triples.
+            - input: Turtle file
+            - output: (Consistency Percentage Dbpedia, Consistency Percentage Wikidata)
     """
 
     g = Graph()
     g.parse(turtle_file, format="turtle")
 
-    consistency_state = []
+    consistency_state_dbpedia = []
+    
     for subject, predicate, object_ in g.triples((None, None, None)):
-        dbpedia_graph = Graph()
+        consistency_state = get_consistency_state_dbpedia(
+            subject,
+            predicate,
+            object_
+        )
 
-        uri = subject
-        try:
-            predicate = predicate
-            object_ = object_
-            
-            dbpedia_graph.parse(
-                uri
+        consistency_state_dbpedia.append(
+            consistency_state
+        )
+        
+    
+    consistency_state_wikidata = []
+
+    for subject, predicate, object_ in g.triples((None, None, None)):
+        consistency_state = get_consistency_state_wikidata(
+            subject,
+            predicate,
+            object_
+        )
+
+        consistency_state_wikidata.append(
+            consistency_state
+        )
+
+    truth_percentage_dbpedia = (consistency_state_dbpedia.count(True)/(consistency_state_dbpedia.count(True) + consistency_state_dbpedia.count(False))) * 100
+
+    truth_percentage_wikidata = (consistency_state_wikidata.count(True)/(consistency_state_wikidata.count(True) + consistency_state_wikidata.count(False))) * 100
+
+    return (
+        truth_percentage_dbpedia,
+        truth_percentage_wikidata
+    )
+    
+
+
+def get_consistency_state_dbpedia(subject, predicate, object_):
+
+    """
+        Obtain the consistency of the object by querying dbpedia.
+            - input: (subject, predicate, object to reference)
+            - output: boolean
+    """
+
+    dbpedia_graph = Graph()
+
+    try:
+        dbpedia_graph.parse(
+            subject
+        )
+
+        all_objects = []
+        for object__ in dbpedia_graph.objects(
+                subject = subject,
+                predicate = predicate,
+                unique = True
+        ):
+            all_objects.append(
+                object__
             )
 
-            all_objects = []
-            for object__ in dbpedia_graph.objects(
-                    subject=uri,
-                    predicate=predicate,
-                    unique=True
-            ):
-                all_objects.append(
-                    object__
-                )
+        if object_ in all_objects:
+            return True
+        else:
+            print("Inconsistency ", object_, all_objects)
+            return False
+    
+        return True
+    except:
+        print("URI failed: ", subject)
+        return False
 
-            if object_ in all_objects:
-                consistency_state.append(
-                    True
-                )
-            else:
-                print("Inconsistency ", object_, all_objects)
-                consistency_state.append(
-                    False
-                ) 
-        except:
-            print("URI failed :", uri)
-            consistency_state.append(False)
+    
+def get_consistency_state_wikidata(subject, predicate, object_):
 
-    truth_percentage = consistency_state.count(True)/(consistency_state.count(True) + consistency_state.count(False)) * 100
+    """
+        Obtain the consistency of the object by querying dbpedia.
+            - input: (subject, predicate, object to reference)
+            - output: boolean
+    """
 
-    return truth_percentage
+    # Find mapping from dbpedia
+    dbpedia_graph_subject = Graph()
+    try:
+        dbpedia_graph_subject.parse(
+            subject
+        )
+    except:
+        print("URI failed wikidata subject parse: ", subject)
+        return False
+
+    wikidata_ref_subject = []
+    for objects in dbpedia_graph_subject.objects(
+            subject,
+            OWL.sameAs,
+            unique=True
+    ):
+        if "wikidata" in objects:
+            wikidata_ref_subject.append(
+                objects
+            )
+
+    dbpedia_graph_predicate = Graph()
+    try:
+        dbpedia_graph_predicate.parse(
+            predicate
+        )
+    except:
+        print("URI failed wikidata predicate parse: ", predicate)
+        return False
+
+    wikidata_ref_predicate = []
+    for objects in dbpedia_graph_predicate.objects(
+            predicate,
+            OWL.equivalentProperty,
+            None
+    ):
+        if "wikidata" in objects:
+            wikidata_ref_predicate.append(
+                objects
+            )
+
+    dbpedia_graph_object = Graph()
+    print("The object is")
+    print(object_)
+    try:
+        dbpedia_graph_object.parse(
+            object_
+        )
+    except:
+        print("URI failed wikidata object parse: ", object_)
+        return False
+
+    wikidata_ref_object = []
+    for objects in dbpedia_graph_object.objects(
+            object_,
+            OWL.sameAs,
+            None
+    ):
+        if "wikidata" in objects:
+            wikidata_ref_object.append(
+                objects
+            )
+
+    print("The subject predicate and object is")
+    print(wikidata_ref_subject)
+    print(wikidata_ref_predicate)
+    print(wikidata_ref_object)
+
+    wikidata_graph = Graph()
+    wikidata_graph.parse(
+        wikidata_ref_subject[0]
+    )
+
+    all_objects = []
+    for s, p, o in wikidata_graph.triples(
+            (
+                None,
+                None,
+                None
+            )
+    ):
+        if ("/prop/"+wikidata_ref_predicate[0].split("/")[-1] in p) and (s == wikidata_ref_subject[0]):
+            all_objects.append(
+                o
+            )
+
+    consistency_found = False
+    for object_ in all_objects:
+        if wikidata_ref_object[0] in object_:
+            consistency_found = True
+            return True
+
+    if not consistency_found:
+        print("Inconsistency ", wikidata_ref_object[0], all_objects)
+        return False
+    else:
+        return True
